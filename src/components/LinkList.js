1e9d8c2a7bf46b8b10d776b1b12547d83d3794cb
timebandit <imran.nazir@yahoo.co.uk>
2020-11-27 07:30:46 +0000

    part way through login functionality

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
 import gql from "graphql-tag";
+import React, { Component } from "react";
 import { Query } from "react-apollo";
-import Link from './Link'
+import Link from "./Link";
 
 const FEED_QUERY = gql`
   {
@@ -14,29 +14,29 @@ const FEED_QUERY = gql`
       }
     }
   }
-`
+`;
 
 class LinkList extends Component {
-  
   render() {
-    
     return (
       <Query query={FEED_QUERY}>
         {({ loading, error, data }) => {
-          if (loading) return <div>Fetching</div>
-          if (error) return <div>Error</div>
-    
-          const linksToRender = data.feed.links
-    
+          if (loading) return <div>Fetching</div>;
+          if (error) return <div>Error</div>;
+
+          const linksToRender = data.feed.links;
+
           return (
             <div>
-              {linksToRender.map(link => <Link key={link.id} link={link} />)}
+              {linksToRender.map((link) => (
+                <Link key={link.id} link={link} />
+              ))}
             </div>
-          )
+          );
         }}
       </Query>
-    )
+    );
   }
 }
 
-export default LinkList
+export default LinkList;
